1abb374db723bff5d0b7b145805bf86552aec280
h26n <likelion.leedh@gmail.com>
2026-08-19 13:54:15 +0900

    init

---
+++ src/main/java/com/example/ktb_hktn_team2/KtbHktnTeam2Application.java
@@ -0,0 +1,13 @@
+package com.example.ktb_hktn_team2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtbHktnTeam2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KtbHktnTeam2Application.class, args);
+	}
+
+}
